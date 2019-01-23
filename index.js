d9a9789f1db0fd7f2c04b404bd44c06b83c4fb98
mjm03 <mjm03@hampshire.edu>
2019-01-23 00:06:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string)  {
+  return string.toUpperCase()
+}
+function whisper(string)  {
+  return string.toLowerCase()
+}
+function logShout ()  {
+  console.log("HELLO")
+}
+
+function logWhisper ()  {
+  console.log("hello")
+}
+
+function sayHiToGrandma(string)  {
+var lowercase = "I can't hear you!"
+lowercase.toLowerCase() === lowercase
+
+if (string === lowercase){
+  return "I can't hear you!"
+}
+  
+
+}
